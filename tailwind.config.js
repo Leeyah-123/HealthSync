bfde0c3a767a222960e3e60bb0df4ce5aff6cbd9
Victory Odumeh <vodumeh@gmail.com>
2023-10-06 21:10:25 +0100

    feat: added styles to the signup and signin forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
 }
 
