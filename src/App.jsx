44cb8fa478ba91c0a6fd2a0f72a73833a18c3698
Amadi Jerry <amadijerry310@gmail.com>
2023-10-07 10:39:39 +0100

    moved routing to main.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,20 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  createRoutesFromElements,
-  Navigate
-} from "react-router-dom";
-import DashboardPage from "./pages/dashboard/DashboardPage";
-import Home from "./pages/dashboard/Home";
-import Forum from "./pages/dashboard/Forum";
-import Settings from "./pages/dashboard/Settings";
-import Calendar from "./pages/dashboard/Calendar";
-
-import {ChakraProvider} from "@chakra-ui/react"
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
-
-  const router  = createBrowserRouter(
-
-    createRoutesFromElements(
-      <>
-        <Route path="/">
-          <Route path="dashboard" element={ <DashboardPage /> }>
-            <Route index element={<Navigate to="home" replace />} />
-            <Route path="home" element={ <Home /> } />
-            <Route path="forum" element={ <Forum /> } />
-            <Route path="calendar" element={<Calendar />} />
-            <Route path="settings" element={ <Settings /> } />
-          </Route>
-        </Route>
-      </>
-    )
-
-  )
-
   return (
-    <ChakraProvider >
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  )
+    <>
+      <Navbar />
+
+      <main>
+        <Outlet />
+      </main>
+
+      {/* Footer should go here */}
+      {/* <Footer /> */}
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import AuthLayout from './components/layout/auth/auth.layout';
+import './index.css';
+import Signin from './pages/auth/signin';
+import Signup from './pages/auth/signup';
+import DashboardPage from "./pages/dashboard/DashboardPage";
+import Home from "./pages/dashboard/Home";
+import Forum from "./pages/dashboard/Forum";
+import Settings from "./pages/dashboard/Settings";
+import Calendar from "./pages/dashboard/Calendar";
+import { Navigate } from 'react-router-dom';
+import {ChakraProvider} from "@chakra-ui/react"
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      // Landing Page should go here
+      // {
+      //   index: true,
+      //   element: <Home />,
+      // },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      { path: 'signup', element: <Signup /> },
+      { path: 'login', element: <Signin /> },
+    ],
+  },
+  {
+    path: '/dashboard',
+    element: <DashboardPage />,
+    children: [
+      {index: true, element: <Navigate to="/dashboard/home" replace />},
+      {path: "/dashboard/home", element: <Home />},
+      {path: "/dashboard/forum", element: <Forum />},
+      {path: "/dashboard/calendar", element: <Calendar />},
+      {path: "/dashboard/settings", element: <Settings />},
+
+    ]
+  }
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider >
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
