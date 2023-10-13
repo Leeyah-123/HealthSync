50824152c633abe2ad65721d54d2253e2a7d289e
Amadi Jerry <amadijerry310@gmail.com>
2023-10-13 15:01:14 +0100

    added chakra provider and fixed error in dashboard provider

--- src/routes/dashboard.routes.jsx
+++ src/routes/dashboard.routes.jsx
@@ -1,14 +1,17 @@
+import { Navigate } from 'react-router-dom';
 import DashboardPage from '../pages/dashboard/DashboardPage';
 import Forum from '../pages/dashboard/Forum';
 import DashboardHome from '../pages/dashboard/Home';
 import Schedule from '../pages/dashboard/Schedule';
 import Settings from '../pages/dashboard/Settings';
 
+
 const DashboardRoutes = {
   path: '/dashboard',
   element: <DashboardPage />,
   children: [
-    { index: true, element: <DashboardHome /> },
+    { index: true, element: <Navigate to="home" replace /> },
+    { path: 'home', element: <DashboardHome /> },
     { path: 'forum', element: <Forum /> },
     { path: 'calendar', element: <Schedule /> },
     { path: 'settings', element: <Settings /> },
