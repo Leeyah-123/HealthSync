e725d375f3cc59e0b268602864aee83d0410f068
Salvation Amoke <salvationamoke@gmail.com>
2023-10-18 13:03:45 +0100

    contactpage hero 	modified:   src/components/compound/contactmain.jsx 	new file:   src/components/simple/contactpage/component/Conthero.jsx

--- src/components/compound/contactmain.jsx
+++ src/components/compound/contactmain.jsx
@@ -1,9 +1,11 @@
 import Contheader from "../simple/contactpage/Contheader"
 import Contfooter from "../simple/contactpage/Contfooter"
+import Conthero from "../simple/contactpage/component/Conthero"
 const Contactmain = () => {
     return (
         <>
             <Contheader />
+            <Conthero />
             <Contfooter />
         </>
     )
---
+++ src/components/simple/contactpage/component/Conthero.jsx
@@ -0,0 +1,10 @@
+const Conthero = () => {
+    return (
+        <section className="text-center space-y-2 mx-2">
+            <h1 className="text-lemon font-bold text-3xl  capitalize">contact us</h1>
+            <h3>Get in touch and let us know how we can help you</h3>
+        </section>
+    )
+}
+
+export default Conthero
