f625df092360b43450902d4119d82a2f533f78d7
Leeyah-123 <junaidaaliyah260@gmail.com>
2023-10-20 20:03:58 +0100

    Refactor contact page

--- src/components/simple/contact/ContactForm.jsx
+++ src/components/simple/contact/ContactForm.jsx
@@ -1,41 +1,65 @@
+import { useToast } from '@chakra-ui/react';
 import { useState } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
 const ContactForm = () => {
-  const [name, setname] = useState('')
-  const [message, setmessage] = useState('')
-  const [email, setemail] = useState('')
-  const [toast, settoast] = useState(false)
-  const change = (e) => (e.target.value)
+  const initialState = { name: '', email: '', message: '', topic: 'workout' };
+  const [form, setForm] = useState(initialState);
+  const toast = useToast();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!form.name || !form.email || !form.message || !form.topic)
+      return toast({
+        title: 'Please fill in all required fields',
+        status: 'error',
+      });
+
+    toast({
+      title:
+        'Form submitted successfully. We will reach back to you as soon as possible.',
+      status: 'success',
+    });
+  };
+
   return (
-    <form className="w-3/4 mx-auto space-y-5 pt-8 sm:pt-12 font-semibold text-lightblack">
+    <form
+      className="w-3/4 mx-auto space-y-5 pt-8 sm:pt-12 font-semibold text-lightblack"
+      onSubmit={handleSubmit}
+    >
       <div className="sm:flex capitalize sm:space-x-10">
         <div className="grid">
           <label htmlFor="name">name:</label>
           <input
-            value={name}
-            onChange={(e) => setname(change(e))}
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
             id="name"
             type="text"
             className="border-b-2 outline-none border-lightblack p-3"
+            required
           />
         </div>
         <div className="grid w-full">
           <label htmlFor="email">email:</label>
           <input
-            value={email}
-            onChange={(e) => setemail(change(e))}
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
             id="email"
             type="email"
             className="border-b-2 outline-none border-lightblack p-3"
+            required
           />
         </div>
       </div>
       <div className="grid gap-2 capitalize">
         <label htmlFor="topic">topic</label>
         <select
+          value={form.topic}
+          onChange={(e) => setForm({ ...form, topic: e.target.value })}
           id="topic"
           className="border-b-2 outline-none border-lightblack capitalize p-3"
+          required
         >
           <option value="workout">workout</option>
           <option value="nutrition">nutrition</option>
@@ -47,32 +71,18 @@ const ContactForm = () => {
           your message
         </label>
         <textarea
-          value={message}
-          onChange={(e) => setmessage(change(e))}
+          value={form.message}
+          onChange={(e) => setForm({ ...form, message: e.target.value })}
           id="message"
           rows={3}
           className="border-b-2 outline-none border-lightblack"
+          required
         />
       </div>
 
-      {toast &&
-        <div className=' text-green uppercase font-bold'>
-          <p>Thank you for contacting us</p>
-          <p>your message has been succeffully submitted.</p>
-        </div>
-      }
       <button
         className="transition-all capitalize text-sm flex space-x-2 py-2.5 px-2 relative before:absolute before:content-[''] before:w-1/2 before:inset-0 before:bg-lemon hover:before:w-full before:transition-all"
         type="submit"
-        onClick={(e) => (
-          e.preventDefault(),
-          (name && email && message) ? (
-            settoast(!toast),
-            setmessage(''),
-            setname(''),
-            setemail('')
-          ) : alert('Please Fill The Contact Form',)
-        )}
       >
         <span className="z-10">send message</span>
         <FaArrowRight className="self-center z-10" />
